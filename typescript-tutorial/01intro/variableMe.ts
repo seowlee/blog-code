1062ca6172d3afbb8bb018de562f57cc39bae7b3
Seo Woo Lee <tegfsl@gmail.com>
2024-02-07 00:44:34 +0900

    FEAT: variable declaration with type

--- typescript-tutorial/01intro/variableMe.ts
+++ typescript-tutorial/01intro/variableMe.ts
@@ -1,8 +1,24 @@
 let greetings: string = "Hello Hitesh";
 
+greetings.toUpperCase();
 console.log(greetings);
 
 let mynum = 6;
 // mynum.toUpperCase();
+
+// number
+
+// Bad
+// let userId: number = 334455;
+
+// Good
+let userId = 334455;
+userId.toFixed();
+userId = 'hhhh';
+
+
+
+// boolean
+let isLoggedIn: boolean = false;
 
 export {}
